bbe668da920ada077ac8c6d29ddb9708795d658f
Chaitanya <achaitanya@internal.tavisca.com>
2019-08-14 12:19:43 +0530

    After testing api

---
+++ SimpleCoreWebAPI/Controllers/GreetController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetController : ControllerBase
+    {
+        [HttpGet("Talk")]
+        public string Talk(string message)
+        {
+            if (message.ToLower() == "hi")
+                return "Hello";
+
+            if (message.ToLower() == "hello")
+                return "Hi";
+
+            return "I dont know what you are saying";
+        }
+    }
+}
